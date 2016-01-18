e545192c86ad27b8c8f38c19791f87e198ec8e86
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2016-01-17 17:44:51 -0800

    Added relationships between db tables in models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  has_many :questions
+  has_many :answers
+  has_many :posts
 end
