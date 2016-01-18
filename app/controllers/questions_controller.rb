12fa619b274b3caf8faaed1ad9e0b5f7bb400863
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2016-01-18 12:36:24 -0800

    Generated Question controller.  Updated views for Question Index and Show.  Updated routes to simplfy using resource.

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def index
+  	@questions = Question.all
+  end
+
+  def show
+  	@question = Question.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
