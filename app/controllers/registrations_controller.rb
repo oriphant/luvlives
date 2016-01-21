07b39377afc0083dfc7af2e419e04f6db33ef734
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2016-01-21 14:24:11 -0800

    Finished registration edit page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
+  
 
   protected
   # Sets route for Devise after sign-up
@@ -8,10 +9,15 @@ class RegistrationsController < Devise::RegistrationsController
     confirmation_user_path(resource.id)
   end
 
+  # Sets route for after updating profile
+  def after_update_path_for(resource)
+    questions_path
+  end
+
   # Determines what variable can be saved to the database
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :city, :bio, :email, :password, :password_confirmation, :current_password)}
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :status, :bio, :city, :state, :gender, :age, :website, :facebook, :twitter, :linkedin)}  
   end
 
 end
